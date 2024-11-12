a5a32260b53a2b0742297e4cdf4dd352b03a5fc3
Reptudn <densetsu467@gmail.com>
2024-11-12 16:06:52 +0100

    no progress

--- srcs/requirements/mariadb/tools/start_mariadb.sh
+++ srcs/requirements/mariadb/tools/start_mariadb.sh
@@ -1,3 +1,13 @@
-echo hi
+#!/bin/bash
+
+sudo service mysql start
 
 mysqld
+
+sleep 5
+
+echo db name is $MARIA_DB_DATABASE_NAME
+
+mysql -u root -e "CREATE DATABASE IF NOT EXISTS $MARIA_DB_DATABASE_NAME"
+
+tail -f /dev/null
