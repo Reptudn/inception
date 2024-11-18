ef24286054300ba9663a5e1add06da934e19df08
jkauker <jkauker@student.42heilbronn.de>
2024-11-18 10:26:22 +0100

    it doesnt work

--- srcs/requirements/mariadb/tools/start_mariadb.sh
+++ srcs/requirements/mariadb/tools/start_mariadb.sh
@@ -12,20 +12,15 @@ until mysqladmin ping; do
     sleep 1
 done
 
-echo $MYSQL_ROOT_USER
-echo $MYSQL_ROOT_PASSWORD
-echo $MYSQL_DATABASE
+echo root user for db is:  $MYSQL_ROOT_USER
+echo root user pass is: $MYSQL_ROOT_PASSWORD
+echo db name is: $MYSQL_DATABASE
 
 echo "MariaDB is ready!"
-# echo "Creating database ${MYSQL_DATABASE}..."
-# mysql -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};"
-# mysql -e "CREATE USER IF NOT EXISTS '${MYSQL_ROOT_USER}'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
-# mysql -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_ROOT_USER}'@'%' WITH GRANT OPTION;"
-# mysql -e "FLUSH PRIVILEGES;"
-
-mysql -e "CREATE DATABASE IF NOT EXISTS wordpress;"
-mysql -e "CREATE USER IF NOT EXISTS 'master'@'%' IDENTIFIED BY 'mainframe';"
-mysql -e "GRANT ALL PRIVILEGES ON wordpress.* TO 'master'@'%' WITH GRANT OPTION;"
+echo "Creating database ${MYSQL_DATABASE}..."
+mysql -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};"
+mysql -e "CREATE USER IF NOT EXISTS '${MYSQL_ROOT_USER}'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
+mysql -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_ROOT_USER}'@'%' WITH GRANT OPTION;"
 mysql -e "FLUSH PRIVILEGES;"
 
 echo "Database ${MYSQL_DATABASE} created!"
