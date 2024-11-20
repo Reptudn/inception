1980554b5b85b7a440d2951d30f1edae06c69100
jkauker <jkauker@student.42heilbronn.de>
2024-11-20 15:09:29 +0100

    no syntax err in init.sql

--- srcs/requirements/nginx/tools/start_nginx.sh
+++ srcs/requirements/nginx/tools/start_nginx.sh
@@ -2,4 +2,9 @@ openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
 	-keyout /etc/nginx/certs/ssl_key.key -out /etc/nginx/certs/ssl_cert.crt \
 	-subj "/C=DE/ST=BW/L=HN42/O=42Heilbronn/CN=$DOMAIN"
 
+echo "Starting Nginx..."
 nginx -g 'daemon off;'
+echo "Nginx is ready!"
+
+# Keep the container running
+tail -f /dev/null
