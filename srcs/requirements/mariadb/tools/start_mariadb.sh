2dafbe4adc698c5027a04dd4cdd0550dceae133f
jkauker <jkauker@student.42heilbronn.de>
2024-11-22 09:51:54 +0100

    changed everything to be simpler with just including the .env file

--- srcs/requirements/mariadb/tools/start_mariadb.sh
+++ srcs/requirements/mariadb/tools/start_mariadb.sh
@@ -5,25 +5,26 @@ mkdir -p /run/mysqld
 chown mysql:mysql /run/mysqld
 
 # Start MariaDB in the background
-mysqld --init-file=/etc/mysql/init.sql
+mysqld & # --init-file=/etc/mysql/init.sql &
 
 echo "Waiting for MariaDB to be ready..."
-until mysqladmin ping; do
-    sleep 1
+# Add a delay to ensure MariaDB has time to start
+sleep 10
+
+# Improved readiness check
+until mysqladmin ping --silent; do
+    echo "Waiting for database connection..."
+    sleep 2
 done
 
-echo root user for db is:  $MYSQL_ROOT_USER
-echo root user pass is: $MYSQL_ROOT_PASSWORD
-echo db name is: $MYSQL_DATABASE
-
 echo "MariaDB is ready!"
-echo "Creating database ${MYSQL_DATABASE}..."
-mysql -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};"
-mysql -e "CREATE USER IF NOT EXISTS '${MYSQL_ROOT_USER}'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
-mysql -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_ROOT_USER}'@'%' WITH GRANT OPTION;"
+echo "Creating database ${MARIA_DB_DATABASE_NAME}..."
+mysql -e "CREATE DATABASE IF NOT EXISTS ${MARIA_DB_DATABASE_NAME};"
+mysql -e "CREATE USER IF NOT EXISTS '${MARIA_DB_ROOT_USER}'@'%' IDENTIFIED BY '${MARIA_DB_ROOT_PASSWORD}';"
+mysql -e "GRANT ALL PRIVILEGES ON ${MARIA_DB_DATABASE_NAME}.* TO '${MARIA_DB_ROOT_USER}'@'%' WITH GRANT OPTION;"
 mysql -e "FLUSH PRIVILEGES;"
 
-echo "Database ${MYSQL_DATABASE} created!"
+echo "Database ${MARIA_DB_DATABASE_NAME} created!"
 
 # Keep the container running
 tail -f /dev/null
--- srcs/requirements/nginx/tools/start_nginx.sh
+++ srcs/requirements/nginx/tools/start_nginx.sh
@@ -1,10 +1,24 @@
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-	-keyout /etc/nginx/certs/ssl_key.key -out /etc/nginx/certs/ssl_cert.crt \
-	-subj "/C=DE/ST=BW/L=HN42/O=42Heilbronn/CN=$DOMAIN"
+#!/bin/bash
 
+# Generate SSL certificate and key
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
+    -keyout /etc/nginx/certs/ssl_key.key -out /etc/nginx/certs/ssl_cert.crt \
+    -subj "/C=DE/ST=BW/L=HN42/O=42Heilbronn/CN=$DOMAIN"
+
+# Check if the certificate and key files are created
+if [ ! -f /etc/nginx/certs/ssl_key.key ] || [ ! -f /etc/nginx/certs/ssl_cert.crt ]; then
+    echo "Error: SSL certificate or key file not found!"
+    exit 1
+fi
+
+# Test NGINX configuration
+nginx -t
+if [ $? -ne 0 ]; then
+    echo "Error: NGINX configuration test failed!"
+    exit 1
+fi
+
+# Start NGINX
 echo "Starting Nginx..."
 nginx -g 'daemon off;'
-echo "Nginx is ready!"
-
-# Keep the container running
-tail -f /dev/null
+echo "Nginx exited!"
