7a448d025ed26eb09d713de04949908f37991989
Jonas <jonas.kauker2003@gmail.com>
2024-12-11 09:36:17 +0100

    added svelete kit app for the custom page + changed npm server to server from the public directory of the svelte kit app

--- srcs/requirements/bonus/custom_page/tools/server.js
+++ srcs/requirements/bonus/custom_page/tools/server.js
@@ -4,7 +4,7 @@ const app = express();
 
 const PORT = 4242;
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'app/public')));
 
 app.listen(PORT, () => {
 	console.log(`Server is running on http://localhost:${PORT}`);
